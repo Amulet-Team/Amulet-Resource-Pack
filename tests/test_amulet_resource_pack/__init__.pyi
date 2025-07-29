7eb6f5fb79a72a85f3cbbcc105bfa8115a355079
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-07-29 10:15:25 +0100

    Update stubs

--- tests/test_amulet_resource_pack/__init__.pyi
+++ tests/test_amulet_resource_pack/__init__.pyi
@@ -1,15 +1,11 @@
 from __future__ import annotations
 
-import faulthandler as faulthandler
+import faulthandler as _faulthandler
 
 from . import _test_amulet_resource_pack
 
-__all__ = ["compiler_config", "faulthandler"]
+__all__ = ["compiler_config"]
 
 def _init() -> None: ...
 
-compiler_config: dict = {
-    "pybind11_version": "2.13.6",
-    "compiler_id": "MSVC",
-    "compiler_version": "19.43.34808.0",
-}
+compiler_config: dict
